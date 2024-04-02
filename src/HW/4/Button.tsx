b1659c5ba8d292e21e37ab088515dcf6dc011f2b
kirill <da.deffoxz@gmail.com>
2024-04-02 20:25:03 +0300

    добавил 4 дз

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,13 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
     // НУЖНО ДОПИСАТЬ
+    props.callBack()
+
   };
 
   return (
